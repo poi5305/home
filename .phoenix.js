6bd53a3da8b85d77cc3ba3f7e208a50ff53c411f
poi5305 <poi5305@gmail.com>
2017-09-09 23:03:09 +0800

    new version

--- .phoenix.js
+++ .phoenix.js
@@ -2,61 +2,15 @@ console.log = function(...args){
   Phoenix.log(...args);
 };
 
-function getWindowCount(screenId) {
-  const screens = Screen.all();
-  const windowCount = {};
-  for (let screen of screens) {
-    if (screen.identifier() != screenId) {
-      continue;
-    }
-    const windows = screen.windows();
-    for (let window of windows) {
-      const frame = window.frame();
-      if (frame.width > 1 && frame.height > 1) {
-        const app = window.app();
-        const appName = app.bundleIdentifier();
-        if (windowCount[appName] === undefined) {
-          windowCount[appName] = 1;
-        } else {
-          windowCount[appName]++;
-        }
-      }
-    }
-  }
-  return windowCount;
-}
-
-function adjustWindow(layout) {
-  const screens = Screen.all();
-  for (let screen of screens) {
-    const layoutWindows = layout[screen.identifier()];
-    if (layoutWindows === undefined) {
-      continue;
-    }
-    const windows = screen.windows();
-    for (let window of windows) {
-      const frame = window.frame();
-      if (frame.width < 2 && frame.height < 2) {
-        continue;
-      }
-      const app = window.app();
-      const appName = app.bundleIdentifier();
-      const frames = layoutWindows[appName];
-      if (frames === undefined) {
-        // window.minimise();
-      } else {
-        if (frames.length > 0) {
-          window.setFrame(frames.shift());
-        } else {
-          // window.minimise();
-        }
-      }
-    }
+function listApps() {
+  const apps = App.all();
+  for (let app of apps) {
+    console.log(app.bundleIdentifier());
   }
 }
 
-function save(number) {
-  const layout = {};
+function getAppsFromScreens() {
+  const apps = [];
   const screens = Screen.all();
   for (let screen of screens) {
     const layoutWindows = {};
@@ -66,55 +20,73 @@ function save(number) {
       const appName = app.bundleIdentifier();
       const frame = window.frame(); 
       if (frame.width > 1 && frame.height > 1) {
-        if (layoutWindows[appName] === undefined) {
-          layoutWindows[appName] = [];
-        }
-        layoutWindows[appName].push(frame);
+        apps.push({
+          app: app,
+          window: window,
+          name: appName,
+          frame: frame,
+        });
       }
     }
-    layout[screen.identifier()] = layoutWindows;
   }
-  Storage.set(`record_${number}`, layout);
-  Phoenix.log(JSON.stringify(layout));
+  return apps;
+}
+
+const LaunchSettings = {
+  'com.microsoft.VSCode': ['-n', '-b', 'com.microsoft.VSCode', '--args', '.'],
+  'com.google.Chrome': ['-n', '-b', 'com.google.Chrome'],
+  'com.apple.Terminal': ['-b', 'com.apple.Terminal', ""],
+};
+
+function save(number) {
+  const apps = getAppsFromScreens();
+  const saveApps = [];
+  for (let app of apps) {
+    saveApps.push({
+      name: app.name,
+      frame: app.frame,
+    })
+  }
+  console.log(JSON.stringify(saveApps));
+  Storage.set(`record_${number}`, saveApps);
+}
+
+function adjustWindow(apps) {
+  const currentApps = getAppsFromScreens();
+  for(let cApp of currentApps) {
+    let removeIdx = -1;
+    for (let i in apps) {
+      const app = apps[i];
+      if (cApp.name === app.name) {
+        cApp.window.setFrame(app.frame);
+        removeIdx = i;
+      }
+    }
+    apps.splice(removeIdx, 1);
+  }
 }
 
 function load(number) {
-  const layout = Storage.get(`record_${number}`);
-  console.log(JSON.stringify(layout));
-  if (layout === undefined) {
+  const apps = Storage.get(`record_${number}`);
+  if (apps === undefined) {
     return;
   }
-  const workScreen = Window.focused().screen().identifier();
-  console.log(workScreen);
-  const layoutWindows = layout[workScreen];
-  if (layoutWindows === undefined) {
-    return;
-  }
-
-  const windowCount = getWindowCount(workScreen);
-  let count = 0;
-  
-  for (let appName in layoutWindows) {
-    const frames = layoutWindows[appName];
-    const currentWindowCount = windowCount[appName] || 0;
-    for (let i = 0; i < frames.length - currentWindowCount; i++) {
-      count++;
-      const args = ['-n', '-b', appName];
-      if (appName == 'com.microsoft.VSCode') {
-        args.push('--args');
-        args.push('.');
-      }
-      const task = new Task('/usr/bin/open', args, (r) => {
-        count--;
-        if (count == 0) {
-          Timer.after(1, () => {adjustWindow(layout);});
-        }
-      });
+  adjustWindow(apps);
+  console.log(JSON.stringify(apps));
+  // launch app
+  let waitCount = 0;
+  for (let app of apps) {
+    waitCount++;
+    let args = LaunchSettings[app.name];
+    if (args === undefined) {
+      args = ['-n', '-b', app.name];
     }
-  }
-  
-  if (count == 0) {
-    adjustWindow(layout);
+    const task = new Task('/usr/bin/open', args, (r) => {
+      waitCount--;
+      if (waitCount === 0) {
+        Timer.after(1, () => {adjustWindow(Storage.get(`record_${number}`));});  
+      }
+    });
   }
 }
 
@@ -123,7 +95,7 @@ Key.on('1', ['ctrl', 'alt', 'shift'], () => {save('1');});
 Key.on('2', ['ctrl', 'alt', 'shift'], () => {save('2');});
 Key.on('3', ['ctrl', 'alt', 'shift'], () => {save('3');});
 Key.on('4', ['ctrl', 'alt', 'shift'], () => {save('4');});
-// load
+// // load
 Key.on('1', ['ctrl', 'alt'], () => {load('1')});
 Key.on('2', ['ctrl', 'alt'], () => {load('2')});
 Key.on('3', ['ctrl', 'alt'], () => {load('3')});
